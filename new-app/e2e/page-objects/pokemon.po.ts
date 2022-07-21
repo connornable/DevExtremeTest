d78a0678942bde257dd3bb493b83a2af481ce779
sailaja.chavali <sailaja.chavali@n-able.com>
2022-07-21 09:47:25 +0100

    New code added

--- new-app/e2e/page-objects/pokemon.po.ts
+++ new-app/e2e/page-objects/pokemon.po.ts
@@ -1,4 +1,4 @@
-import { Atom, PageActions, TreeViewAtom,TableAtom,PaginatorAtom,PopupAtom} from "@n-able/atoms";
+import { Atom, PageActions, TreeViewAtom,TableAtom,PaginatorAtom,PopupAtom, ButtonAtom,DialogAtom} from "@n-able/atoms";
 import { Locator, Page } from "@playwright/test";
 import { on } from "events";
 
@@ -7,21 +7,27 @@ export class Pokemon  {
   private readonly navMenu : TreeViewAtom ;
  private readonly table:TableAtom;
   paginator: PaginatorAtom;
-  popup:PopupAtom;
+  dialog:DialogAtom;
+  button:ButtonAtom;
+
   //checkboxAtom:CheckboxAtom;
 
   constructor(private readonly page : Page) {
   this.navMenu =Atom.findIn(TreeViewAtom,this.page.locator("#Tasks"));
   this.table =Atom.findIn(TableAtom,this.page.locator("#pokemontable"));
   this.paginator =Atom.findIn(PaginatorAtom,this.page.locator("#pokemontable"));
-  this.popup=Atom.findIn(PopupAtom,this.page.locator(".dx-popup-content >> nth=1"));
+  this.dialog=Atom.findIn(DialogAtom,this.page.locator(".dx-popup-wrapper"));
+    this.button=Atom.findIn(ButtonAtom,this.page.locator("#Show"));
   }
 get root():Locator{
   return this.page.locator(".dx-overlay-wrapper");
 }
-  // get clickCheckBox():Locator{
-  //   return this.root.locator(".dx-item dx-list-item dx-list-item-selected").nth(0)
-  // }
+  get CheckBox():Locator{
+    return this.root.locator(".dx-scrollview-content >> .dx-item.dx-list-item").nth(0);
+  }
+ async IsDailogOpened(): Promise<boolean> {
+  return this.dialog.isDisplayed();
+ }
     async clickExamples():Promise<void>{
     await this.navMenu.getRootNode(1).click();
   }
@@ -35,7 +41,7 @@ get root():Locator{
   await PageActions.click (this.table.getCell(0,1).locator(".dx-header-filter"));
  }
  async selectCheckbox():Promise<void>{
-  await PageActions.click(this.popup.getPopupBox().locator(".dx-item dx-list-item ").nth(1));
+  await PageActions.click(this.CheckBox);
  }
  async getHeaderRowText(): Promise<string> {
     return this.table.getHeaderRowText();
@@ -49,4 +55,18 @@ async pageNumberLinkClick(pageNumber: number): Promise<void>{
 async pokemsItemsPerPage(itemsPerPage: number | "All"): Promise<void>{
   return this.paginator.setItemsPerPage(itemsPerPage);
 }
+async clickOnPokemonRow(indexNumber:number){
+  return this.table.clickRow(indexNumber);
+}
+async clickOnButton():Promise<void>{
+  return this.button.click();
+}
+async getImagesSource():Promise<string>{
+  const image :Locator = this.dialog.getElement().locator("img")
+return`${await PageActions.getAttribute(image,"src")}`
+}
+async closeDialog():Promise<void>{
+return this.dialog.clickCloseButton();
+}
+
 }
--- new-app/e2e/specs/pokemon.spec.ts
+++ new-app/e2e/specs/pokemon.spec.ts
@@ -15,28 +15,39 @@ test.describe("Task List>", () => {
    test.beforeEach(async () => {
     await pokemon.clickTasks();
   });
+test.afterEach(async () => {
+  await page.waitForTimeout(3000);
+})
+  test
 test ("Click on the Id cell",async () =>{
  await pokemon.clickId();
 })
-test ("Click on the Id filter",async () =>{
- 
-  await pokemon.clickIdFilter();
+test("Click on the Id filter",async () =>{
+ await pokemon.clickIdFilter();
+expect(await pokemon.IsDailogOpened()).toBe(true);
 })
-test("click the checkbox",async () =>{
-  await pokemon.selectCheckbox();
-})
+// test("click the checkbox",async () =>{
+//   await pokemon.selectCheckbox();
+// });
 test("Get the pokemon header row text" , async () =>{
 const expectedText = "Sprite,Id,Name,Height,Type,";
-expect(await pokemon.getHeaderRowText()).toBe(expectedText)
+expect(await pokemon.getHeaderRowText()).toBe(expectedText);
 })
 test("Get the total rows count ",async () =>{
   expect(await pokemon.pokemonRowCount()).toBe(12);
 })
 test("Click on the pagenumbers link ", async () =>{
-await pokemon.pageNumberLinkClick(3);
+await pokemon.pageNumberLinkClick(2);
+
 })
 test("Click on the count page number links",async () =>{
-await pokemon.pokemsItemsPerPage(10);
+await pokemon.pokemsItemsPerPage(20);
+})
+test("click on show button", async () =>{
+  await pokemon.clickOnPokemonRow(4);
+await pokemon.clickOnButton();
+expect(await pokemon.getImagesSource()).toBe("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/23.png");
+await pokemon.closeDialog();
 })
  });
 
